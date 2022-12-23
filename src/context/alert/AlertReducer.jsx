95ff707a9d28387aba3b57f151c218573f4e4ccd
source-in <saurabh.s@spintly.com>
2022-12-23 17:07:25 +0530

    Initial Commit

---
+++ src/context/alert/AlertReducer.jsx
@@ -0,0 +1,14 @@
+const alertRedcer = (state, action) => {
+  switch (action.type) {
+    case 'SET_ALERT':
+      return action.payload;
+
+    case 'REMOVE_ALERT':
+      return null;
+
+    default:
+      return state;
+  }
+};
+
+export default alertRedcer;
---
+++ src/context/github/GithubActions.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: { Authorization: `toekn ${GITHUB_TOKEN}` },
+});
+
+// Search users
+export const searchUsers = async (text) => {
+  const params = new URLSearchParams({
+    q: text,
+  });
+
+  const res = await github.get(`/search/users?${params}`);
+  return res.data.items;
+
+  // eslint-disable-next-line
+};
+
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+  const params = new URLSearchParams({
+    sort: 'created',
+    per_page: 10,
+  });
+
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos?${params}`),
+  ]);
+
+  return { user: user.data, repos: repos.data };
+};
